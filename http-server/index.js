b1ec2c41c75995cd1af4a293f09c4b72a0d31b30
km796 <31836577+km796@users.noreply.github.com>
2022-08-05 17:09:42 +0900

    http api and routing

--- http-server/index.js
+++ http-server/index.js
@@ -3,15 +3,31 @@ const http = require("http");
 const PORT = 3000;
 
 const server = http.createServer((req, res) => {
-  res.writeHead(200, {
-    "Content-Type": "text/plain",
-  });
-  res.end(
-    JSON.stringify({
-      id: 1,
-      name: "Minsu Kang",
-    })
-  );
+  if (req.url === "/friends") {
+    res.writeHead(200, {
+      "Content-Type": "text/plain",
+    });
+    res.end(
+      JSON.stringify({
+        id: 1,
+        name: "Minsu Kang",
+      })
+    );
+  } else if (req.url === "/messages") {
+    res.setHeader("Content-Type", "text/html");
+    res.write("<html>");
+    res.write("<body>");
+    res.write("<ul>");
+    res.write("<li>Hello Minsu!</li>");
+    res.write("<li>Welcom to node.js<li>");
+    res.write("</ul>");
+    res.write("</body>");
+    res.write("</html>");
+    res.end();
+  } else {
+    res.statusCode = 404;
+    res.end();
+  }
 });
 
 server.listen(PORT, () => {
