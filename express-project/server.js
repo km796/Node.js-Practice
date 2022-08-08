e7e2f8605c87a228b8d276321595827ed564d135
km796 <31836577+km796@users.noreply.github.com>
2022-08-08 11:47:13 +0900

    Route parameters

--- express-project/server.js
+++ express-project/server.js
@@ -3,6 +3,33 @@ const app = express();
 
 const PORT = 3000;
 
+const friends = [
+  {
+    id: 1,
+    name: "Albert Einstein",
+  },
+  {
+    id: 2,
+    name: "Isaac Newton",
+  },
+];
+
+app.get("/friends", (req, res) => {
+  res.json(friends);
+});
+
+app.get("/friends/:friendId", (req, res) => {
+  const friendId = req.params.friendId;
+  const friend = friends[friendId];
+  if (friend) {
+    res.json(friend);
+  } else {
+    res.status(404).json({
+      error: "Friend does not exist",
+    });
+  }
+});
+
 app.get("/", (req, res) => {
   res.send("Hello");
 });
