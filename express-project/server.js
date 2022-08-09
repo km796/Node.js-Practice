ada7976bb7d43573d20c43a5b750370dbdd25f6f
km796 <31836577+km796@users.noreply.github.com>
2022-08-09 15:28:55 +0900

    Express routes

--- express-project/server.js
+++ express-project/server.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const app = express();
-const friendsController = require("./controllers/friends.controller");
-const messagesController = require("./controllers/messages.controller");
+
+const friendsRouter = require("./routes/friends.router");
+const messagesRouter = require("./routes/messages.router");
 
 const PORT = 3000;
 
@@ -9,21 +10,17 @@ app.use((req, res, next) => {
   const start = Date.now();
   next();
   const delta = Date.now() - start;
-  console.log(`${req.method} ${req.url} ${delta}ms`);
+  console.log(`${req.method} ${req.baseUrl}${req.url} ${delta}ms`);
 });
 
 app.use(express.json());
 
-app.post("/friends", friendsController.postFriend);
-app.get("/friends", friendsController.getFriends);
-app.get("/friends/:friendId", friendsController.getFriend);
+app.use("/friends", friendsRouter);
+app.use("/messages", messagesRouter);
 
 app.get("/", (req, res) => {
   res.send("Hello");
 });
-
-app.get("/messages", messagesController.getMessages);
-app.post("/messages", messagesController.postMessage);
 
 app.listen(PORT, () => {
   console.log(`Listening on ${PORT}...`);
